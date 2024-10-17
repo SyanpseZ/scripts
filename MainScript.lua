cf7cabd67ce2e0615a02176cfc450cca9e285a04
SyanpseZ <harrysrobloxacc@gmail.com>
2024-10-17 20:42:04 +0100

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 17525285794 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SyanpseZ/scripts/refs/heads/main/Scripts/SillyJujutsuRandomiser.lua"))()
+elseif game.PlaceId == 127587127386958 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SyanpseZ/scripts/refs/heads/main/Scripts/Vision.lua"))()
 else
     print("Not in a valid game")
 end
