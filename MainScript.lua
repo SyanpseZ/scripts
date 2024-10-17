3cc1dcb833c08007bcc0611282321dac9f34e064
SyanpseZ <harrysrobloxacc@gmail.com>
2024-10-17 22:08:45 +0100

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,8 +1,8 @@
-if game.PlaceId == 17525285794 then
+if game.PlaceId == 17525285794 then -- SJR
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SyanpseZ/scripts/refs/heads/main/Scripts/SillyJujutsuRandomiser.lua"))()
-elseif game.PlaceId == 127587127386958 then
+elseif game.PlaceId == 127587127386958 then -- VISION
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SyanpseZ/scripts/refs/heads/main/Scripts/Vision.lua"))()
-elseif game.PlaceId == 16732694052 then
+elseif game.PlaceId == 16732694052 then --FISCH
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SyanpseZ/scripts/refs/heads/main/Scripts/fisch.lua"))()
 else
     print("Not in a valid game")
