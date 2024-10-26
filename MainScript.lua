cd168649fddf046d50e663e342615693f17f2f71
SyanpseZ <harrysrobloxacc@gmail.com>
2024-10-26 23:03:44 +0100

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -4,6 +4,8 @@ elseif game.PlaceId == 127587127386958 then -- VISION
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SyanpseZ/scripts/refs/heads/main/Scripts/Vision.lua"))()
 elseif game.PlaceId == 16732694052 then --FISCH
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SyanpseZ/scripts/refs/heads/main/Scripts/fisch.lua"))()
+elseif game.PlaceId == 904367475 then -- DBSAT
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SyanpseZ/scripts/refs/heads/main/Scripts/DragonBallSaiyansAbilityTest.lua"))()
 else
     print("Not in a valid game")
 end
